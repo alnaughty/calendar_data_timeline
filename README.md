4576fd2f105074ac6e55e254749a3731eeeaa2d7
Zeus <hidarizeus@gmail.com>
2021-05-03 11:58:12 +0800

    add image and example

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 # calendar_timeline
 
-A new Flutter package.
+A new Flutter package used to create and view calendar with ranged time data, supports Mobile(Android, IOS), Web, and Desktop apps.
 
-## Getting Started
+## Image
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+### Web
+![alt text](https://github.com/alnaughty/calendar_data_timeline/blob/master/images/web-full.png)
---
+++ example/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_data_timeline/calendar_data_timeline.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendar Data Timeline Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CalendarDataTimeline(
+
+        data: <CalendarContent>[
+          CalendarContent(name: "Zeus Cajurao", dates: <DateFromTo>[
+            DateFromTo(
+                color: Colors.red,
+                from: DateTime.now(),
+                to: DateTime.now().add(Duration(days: 4))),
+            DateFromTo(
+                color: Colors.blue,
+                from: DateTime.now().subtract(Duration(days: 42)),
+                to: DateTime.now().subtract(Duration(days: 2))),
+            DateFromTo(
+                color: Colors.green,
+                from: DateTime.now().subtract(Duration(days: 10)),
+                to: DateTime.now().add(Duration(days: 6))),
+          ]),
+          CalendarContent(name: "John Doe", dates: <DateFromTo>[
+            DateFromTo(
+                color: Colors.green,
+                from: DateTime.now(),
+                to: DateTime.now().add(Duration(days: 4))),
+            DateFromTo(
+                color: Colors.yellow,
+                from: DateTime.now().subtract(Duration(days: 42)),
+                to: DateTime.now().subtract(Duration(days: 2))),
+            DateFromTo(
+              color: Colors.blue,
+              from: DateTime.parse("2021-02-28"),
+              to: DateTime.now(),)
+          ]),
+        ],
+        settings: HeaderSettings(
+            locale: 'en_US',
+            topColor: Colors.blue,
+            bottomColor: Colors.grey.shade900,
+            sundayColor: Colors.grey.shade300
+        ),
+
+        bodySettings: BodySettings(
+            titleBGColor: Colors.blue,
+            sundayColor: Colors.grey.shade300,
+            backgroundColor: Colors.grey.shade100
+        ),
+      ),
+    );
+  }
+}
